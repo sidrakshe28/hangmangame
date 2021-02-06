171cd97c79fa3770392b6b714c37ce767ba56e4c
Siddhika Rakshe <49247802+sidrakshe28@users.noreply.github.com>
2021-02-06 13:43:59 +0530

    added popup component

---
+++ src/components/Popup.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Popup() {
+    return (
+        <div class="popup-conatiner">
+            <div class="popup">
+                <h2 id="final-message"></h2>
+                <h3 id="final-message-reveal"></h3>
+                <button id="play-button">PLAY AGAIN</button>
+            </div>
+            
+        </div>
+    )
+}
+
+export default Popup
