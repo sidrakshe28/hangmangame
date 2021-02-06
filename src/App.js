4f0adf8af3ba00e04e9eb87e5ba216cc0e702b55
Siddhika Rakshe <49247802+sidrakshe28@users.noreply.github.com>
2021-02-06 12:10:33 +0530

    added header component

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './components/Header';
+
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Header/>
+    </div>
+  );
+}
+
+export default App;
