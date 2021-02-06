296b78dae3d6fc56101a9aeaf7cf83210afa4e9b
Siddhika Rakshe <49247802+sidrakshe28@users.noreply.github.com>
2021-02-06 13:25:07 +0530

    added word component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './components/Header';
 import Figure from './components/Figure';
 import WrongLetters from './components/WrongLetters';
+import Word from './components/Word';
 
 import './App.css';
 
@@ -12,6 +13,7 @@ function App() {
       <div className="game-container">
         <Figure/>
         <WrongLetters/>
+        <Word/>
       </div>
 
     </div>
---
+++ src/components/Word.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Word() {
+    return (
+        <div class= "word" id="word"></div>
+    )
+}
+
+export default Word
