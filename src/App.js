2678b2755fd8c2b7300204290285241ee7d8c55a
Siddhika Rakshe <49247802+sidrakshe28@users.noreply.github.com>
2021-02-14 21:36:15 +0530

    updated wrongletters

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React ,{ useState }from 'react';
 import Header from './components/Header';
 import Figure from './components/Figure';
 import WrongLetters from './components/WrongLetters';
@@ -8,15 +8,25 @@ import './App.css';
 
 const words = ['application','programming','interface','wizard'];
 
+let selectedWord= words[Math.floor(Math.random() * words.length)];
+
+let playable=true;
+
+const correctLetters=[];
+const WrongLetters =[];
 
 function App() {
+  const [playable , setPlayable] = useState(true);
+  const [correctLetters, setCorrectLetters]= useState( []);
+  const [rongLetters, setWrongLetters]= useState( []);
+
   return (
     <div>
       <Header/>
       <div className="game-container">
         <Figure/>
         <WrongLetters/>
-        <Word/>
+        <Word selectedWord={selectedWord} correctLetters ={correctLetters}/>
       </div>
 
     </div>
